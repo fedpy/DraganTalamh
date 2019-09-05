d7797a1fc37ecbd6f9fdd0fedfcfb3130be7c1ed
Ralitsa Dobreva <rally_sl@abv.bg>
2019-09-05 20:07:15 +0100

    StartScreen buttons added

--- DraganTalamh/GUI/StartScreenController.Designer.cs
+++ DraganTalamh/GUI/StartScreenController.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonCreateNewCharacter = new System.Windows.Forms.Button();
+            this.buttonLoadCharacter = new System.Windows.Forms.Button();
+            this.buttonCreateRandomCharacter = new System.Windows.Forms.Button();
+            this.buttonBlankCharacterSheet = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonCreateNewCharacter
+            // 
+            this.buttonCreateNewCharacter.Location = new System.Drawing.Point(195, 64);
+            this.buttonCreateNewCharacter.Name = "buttonCreateNewCharacter";
+            this.buttonCreateNewCharacter.Size = new System.Drawing.Size(174, 48);
+            this.buttonCreateNewCharacter.TabIndex = 0;
+            this.buttonCreateNewCharacter.Text = "Create a new character";
+            this.buttonCreateNewCharacter.UseVisualStyleBackColor = true;
+            // 
+            // buttonLoadCharacter
+            // 
+            this.buttonLoadCharacter.Location = new System.Drawing.Point(195, 145);
+            this.buttonLoadCharacter.Name = "buttonLoadCharacter";
+            this.buttonLoadCharacter.Size = new System.Drawing.Size(174, 48);
+            this.buttonLoadCharacter.TabIndex = 1;
+            this.buttonLoadCharacter.Text = "Load a character";
+            this.buttonLoadCharacter.UseVisualStyleBackColor = true;
+            // 
+            // buttonCreateRandomCharacter
+            // 
+            this.buttonCreateRandomCharacter.Location = new System.Drawing.Point(195, 229);
+            this.buttonCreateRandomCharacter.Name = "buttonCreateRandomCharacter";
+            this.buttonCreateRandomCharacter.Size = new System.Drawing.Size(174, 49);
+            this.buttonCreateRandomCharacter.TabIndex = 2;
+            this.buttonCreateRandomCharacter.Text = "Create a random character";
+            this.buttonCreateRandomCharacter.UseVisualStyleBackColor = true;
+            // 
+            // buttonBlankCharacterSheet
+            // 
+            this.buttonBlankCharacterSheet.Location = new System.Drawing.Point(195, 313);
+            this.buttonBlankCharacterSheet.Name = "buttonBlankCharacterSheet";
+            this.buttonBlankCharacterSheet.Size = new System.Drawing.Size(174, 49);
+            this.buttonBlankCharacterSheet.TabIndex = 3;
+            this.buttonBlankCharacterSheet.Text = "Blank character sheet";
+            this.buttonBlankCharacterSheet.UseVisualStyleBackColor = true;
             // 
             // StartScreenController
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Controls.Add(this.buttonBlankCharacterSheet);
+            this.Controls.Add(this.buttonCreateRandomCharacter);
+            this.Controls.Add(this.buttonLoadCharacter);
+            this.Controls.Add(this.buttonCreateNewCharacter);
             this.Name = "StartScreenController";
             this.Size = new System.Drawing.Size(556, 415);
             this.ResumeLayout(false);
@@ -42,5 +86,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCreateNewCharacter;
+        private System.Windows.Forms.Button buttonLoadCharacter;
+        private System.Windows.Forms.Button buttonCreateRandomCharacter;
+        private System.Windows.Forms.Button buttonBlankCharacterSheet;
     }
 }
